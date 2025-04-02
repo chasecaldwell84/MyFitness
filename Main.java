18529da5898af151840ee0413449382e68dc7fb5
MikeCodes1 <154788412+MikeCodes1@users.noreply.github.com>
2025-04-01 20:11:13 -0500

    Copied dannis sign up class need to implement changes still

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ package MyFitness;
 
 public class Main {
     public static void main(String[] args) {
-        LandingPage.init();
+        SignUp signUp = new SignUp();
+        signUp.setVisible(true);
+        //LandingPage.init();
         //Login.createGUI();
     }
 }
