9da851e2b3cf3ab422b92ae06c03032b93447ca2
MikeCodes1 <154788412+MikeCodes1@users.noreply.github.com>
2025-04-03 13:42:11 -0500

    Got navbar to work realized we need to make every other class a content pane so we can draw over existing app

--- Main.java
+++ Main.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        App app = new App();
+        /*App app = new App();
         LandingPage lp = new LandingPage(app);
-        lp.setVisible(true);
+        lp.setVisible(true);*/
 
         //NOTE testing
+        App app2 = new App();
+        app2.setVisible(true);
         /*Scanner sc = new Scanner(System.in);
         String input;
         input = sc.nextLine();
