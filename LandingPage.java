efe7aea660a0d34fc562842f0a976b52f0668e0f
MikeCodes1 <154788412+MikeCodes1@users.noreply.github.com>
2025-04-03 13:04:50 -0500

    Added a little style to the landing page, not super good tho

--- LandingPage.java
+++ LandingPage.java
@@ -11,10 +11,21 @@ public class LandingPage extends JFrame {
         getContentPane().setLayout(null);
         setSize(500, 500);
         setLocationRelativeTo(null);
+        setLayout(new GridBagLayout());
+
+        JLabel title = new JLabel("Welcome to MyFitness!");
+        title.setFont(new Font("Arial", Font.BOLD, 30));
+        //title.setBounds(70, 10, 500, 50);
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 2;
+        add(title, c);
 
         JButton login = new JButton("Login");
         Login loginPanel = new Login();
-        login.setBounds(250, 235, 100, 30);
+        /*loginPanel.setLayout(new FlowLayout(FlowLayout.LEFT));*/
+        //login.setBounds(150, 235, 100, 30);
         login.addActionListener(e -> {
             loginPanel.setVisible(true);
             if(loginPanel.getAuthenticated()){
@@ -26,14 +37,25 @@ public class LandingPage extends JFrame {
 
         JButton signUp = new JButton("Sign Up");
         SignUp signUpPanel = new SignUp();
-        signUp.setBounds(250, 265, 100, 30);
+        //signUp.setBounds(150, 265, 100, 30);
         signUp.addActionListener(e -> {
             signUpPanel.setVisible(true);
-
         });
 
+        /*pack();*/
+        GridBagConstraints loginPlace = new GridBagConstraints();
+        loginPlace.gridx = 0;
+        loginPlace.gridy = 1;
+        loginPlace.gridwidth = 1;
+        loginPlace.anchor = GridBagConstraints.SOUTHWEST;
+        add(login, loginPlace);
 
-        add(login);
-        add(signUp);
+        GridBagConstraints signUpPlace = new GridBagConstraints();
+        signUpPlace.gridx = 1;
+        signUpPlace.gridy = 1;
+        signUpPlace.gridwidth = 1;
+        signUpPlace.anchor = GridBagConstraints.SOUTHEAST;
+        add(signUp, signUpPlace);
+
     }
 }
