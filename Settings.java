497309d8be5ea25f00c733788ab2d51162ef74ba
MikeCodes1 <154788412+MikeCodes1@users.noreply.github.com>
2025-04-10 16:19:24 -0500

    added nothing

--- Settings.java
+++ Settings.java
@@ -1,18 +1,17 @@
 package MyFitness;
 
+import MyFitness.User.User;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Settings extends JPanel {
-    private User userType = null;
 
     public Settings(App frame) {
         frame.setTitle("Settings");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(frame.getWidth(), frame.getHeight());
         setLayout(new GridBagLayout());
-
-        userType = frame.getUser();
 
         GridBagConstraints c = new GridBagConstraints();
         JLabel title = new JLabel("Settings", JLabel.CENTER);
@@ -22,12 +21,12 @@ public class Settings extends JPanel {
         c.gridwidth = 2;
         add(title, c);
 
-        JLabel username = new JLabel(frame.getUser().userName, JLabel.RIGHT);
-        JLabel pw = new JLabel(frame.getUser().password, JLabel.RIGHT);
+        JLabel username = new JLabel(frame.getUser().getUserName(), JLabel.RIGHT);
+        JLabel pw = new JLabel(frame.getUser().getPassword(), JLabel.RIGHT);
 
-        add(username, c);
-        System.out.println(username.getText() + " :HELP");
-        add(pw, c);
-        System.out.println(pw.getText());
+        add(username);
+        //System.out.println(username.getText() + " :HELP");
+        add(pw);
+        //System.out.println(pw.getText());
     }
 }
